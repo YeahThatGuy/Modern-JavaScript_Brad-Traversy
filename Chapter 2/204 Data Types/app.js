6e1a83ef2fe3429a3f23f330f6fee7d5270b4928
YeahThatGuy <cr.josan@gmail.com>
2019-07-03 15:11:21 +0100

    Working on lesson 4, Data Types

---
+++ Chapter 2/204 Data Types/app.js
@@ -0,0 +1,28 @@
+// Primitive
+
+// String
+const name = "John Doe";
+// Number
+const age = 45;
+// Boolean
+const hasJob = true
+// Null
+const car = null; //will bug and show object type
+// Undefined
+let test;
+// Symbol
+const sym = Symbol();
+
+// Reference
+
+// Array
+const hobbies = ["movies", "music"];
+// Object literal
+const address = {
+    city: "Boston",
+    state: "MA"
+}
+const today = new Date();
+
+console.log(today);
+console.log(typeof today);
