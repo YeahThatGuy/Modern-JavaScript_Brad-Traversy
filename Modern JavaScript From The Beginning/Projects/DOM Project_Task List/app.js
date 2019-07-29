7c34949494707bc17cb22533c2d2ee6b3ec4473c
YeahThatGuy <cr.josan@gmail.com>
2019-07-29 17:48:30 +0100

    Event to remove element(s) from list and filtering (search)

--- Modern JavaScript From The Beginning/Projects/DOM Project_Task List/app.js
+++ Modern JavaScript From The Beginning/Projects/DOM Project_Task List/app.js
@@ -10,8 +10,14 @@ loadEventListeners();
 
 // Load all event listeners
 function loadEventListeners() {
-    // Add task even
+    // Add task event
     form.addEventListener("submit", addTask);
+    // Remove task event
+    taskList.addEventListener("click", removeTask);
+    // Clear all tasks event
+    clearBtn.addEventListener("click", clearTasks);
+    // Filter task events
+    filter.addEventListener("keyup", filterTasks);
 }
 
 // Add task
@@ -42,4 +48,42 @@ function addTask(e) {
     }
 
     e.preventDefault();
+}
+
+// Remove task
+function removeTask(e) {
+    // e.target = what's clicked on
+    if (e.target.parentElement.classList.contains("delete-item")) {
+        if (confirm("Are you sure?")) {
+        // get i element, then a element, then li and remove
+        const icon = e.target.parentElement;
+        icon.parentElement.remove();
+        }
+    }
+}
+
+// Clear all tasks
+function clearTasks(e) {
+    // taskList.innerHTML = "";
+
+    // Faster
+    while (taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+}
+
+// Filter tasks
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll(".collection-item").forEach
+    (function(task) {
+        const item = task.firstChild.textContent;
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/indexOf
+        if (item.toLowerCase().indexOf(text) != -1) {
+            task.style.display = "block";
+        } else {
+            task.style.display = "none";
+        }
+    });
 }
